4f57c51c63bc08cc2f97c6c326145155bc507c33
Satyam <satyamrastogi9936@gmail.com>
2025-03-10 00:40:36 +0530

    first commit

---
+++ src/OrcaCoin.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.26;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract OrcaCoinContract is ERC20 { 
+
+    constructor() ERC20("Orca", "ORC"){
+        
+
+    }
+    function stake() public{
+
+    }
+
+    function unstake() public{
+
+    }
+}
