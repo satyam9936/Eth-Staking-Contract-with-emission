b06de16d750c007a26b637eefa29abb7a5e8dba3
Satyam <satyamrastogi9936@gmail.com>
2025-03-10 21:53:18 +0530

    Add staking functionality and owner management to OrcaCoin contract

--- src/OrcaCoin.sol
+++ src/OrcaCoin.sol
@@ -3,16 +3,21 @@ pragma solidity ^0.8.26;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract OrcaCoinContract is ERC20 { 
+    address stakingContract;
+    address owner;
 
-    constructor() ERC20("Orca", "ORC"){
-        
-
+    constructor(address _stakingContract) ERC20("Orca", "ORC") {
+        stakingContract = _stakingContract;
+        owner = msg.sender;
     }
-    function stake() public{
 
+    function mint(address account, uint256 value) public {
+        require(msg.sender == stakingContract);
+        _mint(account, value);
     }
 
-    function unstake() public{
-
+    function updateStakingcontract(address _stakingContract) public {
+        require(msg.sender == owner);
+        stakingContract = _stakingContract; // Missing semicolon added
     }
 }
--- test/OrcaCoin.t.sol
+++ test/OrcaCoin.t.sol
@@ -1,10 +1,18 @@
 // SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.26;
 
 import "forge-std/Test.sol";
 
 import "src/OrcaCoin.sol";
 
-contract TestContract is Test{
+contract OrcaCoinTest is Test{
+OrcaCoinContract orcaCoin;
+
+function setUp() public{
+orcaCoin = new OrcaCoinContract(address(this));
+}
+function testInitialSupply() public view {
+    assert(orcaCoin.totalSupply()==0);
+}
     
 }
